443656b15e26d64c2febd427cb24b74bf84de80a
Sebastião Neto <167netof@gmail.com>
2022-05-25 14:39:45 -0300

    Conclusao do script, estilos e versao desktop

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  var button = document.querySelector('.submit-button');
+
+  button.addEventListener('click', valor)
+
+  function valor(){
+    var value = document.querySelector('.input-real:checked').value;
+    var feedback_section = document.querySelector('.rating-state-star');
+
+    feedback_section.innerHTML = `
+    <div class="feedback">
+    <img
+      class="image-feedback"
+      src="./assets/images/illustration-thank-you.svg"
+      alt="Thank You"
+    />
+    <p class="result-feedback">You selected ${value} out of 5</p>
+
+    <div class="text-feedback">
+      <h1 class="text-feedback-title">Thank you!</h1>
+      <p class="text-feedback-description">
+        We appreciate you taking the time to give a rating. If you ever
+        need more support, don't hesitate to get in touch!
+      </p>
+    </div>
+  </div>
+    `;
+  }
+});
